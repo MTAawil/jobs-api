ae235fd3aa0f977a49656ef272b2519c87244ca6
Mhammad-Ali-Tawil <mhammad.ali.tawil@gmail.com>
2023-07-18 14:19:23 +0300

    swaggerUI

--- app.js
+++ app.js
@@ -25,13 +25,17 @@ app.use(
     max: 100,
   })
 );
+const swaggerUI=require('swagger-ui-express')
+const YAML=require('yamljs')
+const swaggerDocument=YAML.load('./swagger.yaml')
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 // routes
-app.get('/',(req,res)=>res.send('jobs api'))
+app.get('/',(req,res)=>res.send('<h1>jobs API</h1><a href ="/api-docs">Documentation</a>'))
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authentication, jobsRouter);
 
