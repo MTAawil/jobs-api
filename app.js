36e92be914b0a209d8bce92f9eedc975926ffb5d
Mhammad-Ali-Tawil <mhammad.ali.tawil@gmail.com>
2023-07-18 12:30:20 +0300

    new files

--- app.js
+++ app.js
@@ -25,11 +25,13 @@ app.use(
     max: 100,
   })
 );
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 // routes
+app.get('/',(req,res)=>res.send('jobs api'))
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authentication, jobsRouter);
 
